e19138f94e60eaf456f7acbcbd1ab66bd0862e95
Yuvraj <yuvrajsi9gh@gmail.com>
2025-06-13 13:46:25 +0530

    Create backend-setup.tf

---
+++ backend-setup.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "terraform-alb-state-prinz"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-alb-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
