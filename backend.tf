d1c3153d85416164ad18a329b27c62ab359ae80b
Yuvraj <yuvrajsi9gh@gmail.com>
2025-06-13 13:45:25 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-alb-state-prinz"
+    key            = "alb/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-alb-locks"
+    encrypt        = true
+  }
+}
