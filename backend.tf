d13a453b5aed9cef5de02f4a9e0cc17ca195f887
Yuvraj <yuvrajsi9gh@gmail.com>
2025-06-16 14:30:00 +0530

    Update and rename backend.tf to README.md

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-alb-state-prinz"
-    key            = "alb/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-alb-locks"
-  }
-}
