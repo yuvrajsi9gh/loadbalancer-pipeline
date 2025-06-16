e2b65c1e0caa954bde9786ba511054c8173b220c
Yuvraj <yuvrajsi9gh@gmail.com>
2025-06-16 14:18:24 +0530

    Update main.tf

--- backend-setup/main.tf
+++ backend-setup/main.tf
@@ -1,29 +1,22 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
 }
 
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "terraform-alb-state-prinz"
+  bucket = var.bucket_name
+  force_destroy = true
+}
 
-  versioning {
-    enabled = true
-  }
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.tf_state.id
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  lifecycle {
-    prevent_destroy = true
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
-resource "aws_dynamodb_table" "tf_lock" {
-  name         = "terraform-alb-locks"
+resource "aws_dynamodb_table" "tf_locks" {
+  name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
