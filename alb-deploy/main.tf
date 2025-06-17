004d89a55abe19d015e630ea09b85664b87de917
Yuvraj <yuvrajsi9gh@gmail.com>
2025-06-17 16:33:45 +0530

    Update main.tf for VPC variables

--- alb-deploy/main.tf
+++ alb-deploy/main.tf
@@ -8,31 +8,31 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
 }
 
 module "network" {
   source = "./modules/network"
-  vpc_id = "vpc-0e593f00d0e881c67"
+  vpc_id = var.vpc_id
 }
 
 module "security" {
   source = "./modules/security"
-  vpc_id = "vpc-0e593f00d0e881c67"
+  vpc_id = var.vpc_id
 }
 
 module "ec2" {
   source       = "./modules/ec2"
   subnet_id    = module.network.subnet1_id
   ami_id       = module.network.rhel_ami
-  key_name     = "k8s"
+  key_name     = var.key_name
   instance_cnt = 2
 }
 
 module "alb" {
   source           = "./modules/alb"
   subnet_ids       = [module.network.subnet1_id, module.network.subnet2_id]
-  vpc_id           = "vpc-0e593f00d0e881c67"
+  vpc_id           = var.vpc_id
   instances        = module.ec2.instance_ids
   security_grp_ids = [module.security.alb_sg_id]
 }
