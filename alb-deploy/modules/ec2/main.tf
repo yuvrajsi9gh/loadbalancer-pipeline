da380d6a430b42242ab28957a53b8e3bab34bc54
Yuvraj <yuvrajsi9gh@gmail.com>
2025-06-17 16:21:01 +0530

    Update main.tf

--- alb-deploy/modules/ec2/main.tf
+++ alb-deploy/modules/ec2/main.tf
@@ -1,1 +1,22 @@
+resource "aws_instance" "web" {
+  count         = var.instance_cnt
+  ami           = var.ami_id
+  instance_type = "t2.micro"
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Web-${count.index}"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "Hello from instance ${count.index}" > /var/www/html/index.html
+              EOF
+}
 
